3a5cf199c53d4a008a8ccfea3135077be3e00919
vinicioslop <vinicioslop7@hotmail.com>
2023-05-20 14:57:18 -0300

    Implementa card sobre tecnologias

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,19 @@
 import Image from 'next/image';
 
 /*
-  <Image
-    className={styles.pic_author}
-    src='/pic.png'
-    width={500}
-    height={500}
-    alt="Picture of the author"
-  />
+  
 */
 
 import styles from './page.module.css';
 
-import mapPin from './icons/map-pin.svg';
-import briefcase from './icons/briefcase.svg';
-import github from './icons/github.svg';
-import linkedin from './icons/linkedin.svg';
-import globe from './icons/globe.svg';
-import mail from './icons/mail.svg';
+import myPic from '../images/pic.png';
+
+import mapPin from '../icons/map-pin.svg';
+import briefcase from '../icons/briefcase.svg';
+import github from '../icons/github.svg';
+import linkedin from '../icons/linkedin.svg';
+import globe from '../icons/globe.svg';
+import mail from '../icons/mail.svg';
 
 export default function Home() {
   return (
@@ -28,9 +24,13 @@ export default function Home() {
       <div className={styles.side_information}>
         <div className={styles.pic_card}>
           <div className={styles.pic_image_background}>
-            <div className={styles.pic_image}>
-              Foto
-            </div>
+            <Image
+              className={styles.pic_author}
+              src={myPic}
+              width={130}
+              height={130}
+              alt="Picture of the author"
+            />
           </div>
           <h1 className={styles.pic_name}>Vinícius Lopes Lima</h1>
           <p className={styles.pic_role}>Fullstack Developer</p>
@@ -98,6 +98,23 @@ export default function Home() {
               <p>vinicioslop7@hotmail.com</p>
             </li>
           </ul>
+        </div>
+        <div className={styles.technologies}>
+          <h2 className={styles.title}>
+            Tecnologias
+          </h2>
+          <div className={styles.list_technologies}>
+            <span className={styles.list_technologies_item}>Javascript</span>
+            <span className={styles.list_technologies_item}>ReactJs</span>
+            <span className={styles.list_technologies_item}>.NET</span>
+            <span className={styles.list_technologies_item}>C#</span>
+            <span className={styles.list_technologies_item}>HTML</span>
+            <span className={styles.list_technologies_item}>CSS</span>
+            <span className={styles.list_technologies_item}>Git</span>
+            <span className={styles.list_technologies_item}>GitHub</span>
+            <span className={styles.list_technologies_item}>MySQL</span>
+            <span className={styles.list_technologies_item}>SQlite</span>
+          </div>
         </div>
       </div>
     </main>
